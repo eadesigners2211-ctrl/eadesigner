158c68eb4a39796d777f62d68c18b30d48b11c97
eadesigners2211-ctrl <eadesigners2211@gmail.com>
2026-09-01 15:57:07 -0300

    feat: add default template formats

---
+++ apps/web/src/templates/templateDefaults.ts
@@ -0,0 +1,22 @@
+export const templateFormats = {
+  FEED: {
+    width: 1080,
+    height: 1080,
+    name: 'Feed'
+  },
+  FEED_PLUS: {
+    width: 1080,
+    height: 1350,
+    name: 'Feed Plus'
+  },
+  STORY: {
+    width: 1080,
+    height: 1920,
+    name: 'Stories/Reels'
+  },
+  BANNER_DESKTOP: {
+    width: 1920,
+    height: 390,
+    name: 'Banner Desktop'
+  }
+};
