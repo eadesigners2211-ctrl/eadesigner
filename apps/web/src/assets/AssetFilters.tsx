35f8adf109767ff86708570a48bc92e9289014ab
eadesigners2211-ctrl <eadesigners2211@gmail.com>
2026-09-01 17:07:09 -0300

    Add assets filter component

---
+++ apps/web/src/assets/AssetFilters.tsx
@@ -0,0 +1,11 @@
+export function AssetFilters(){
+  return (
+    <div className="asset-filters">
+      <button>Todos</button>
+      <button>Produtos</button>
+      <button>Logos</button>
+      <button>Fundos</button>
+      <button>Mockups</button>
+    </div>
+  );
+}
