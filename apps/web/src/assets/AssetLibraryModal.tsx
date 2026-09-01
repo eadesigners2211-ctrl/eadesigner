bc1f6fed244724e6234688fd78083c96265c3d79
eadesigners2211-ctrl <eadesigners2211@gmail.com>
2026-09-01 17:08:38 -0300

    feat: add asset library modal foundation

---
+++ apps/web/src/assets/AssetLibraryModal.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function AssetLibraryModal() {
+  return (
+    <div className="asset-library-modal">
+      <h2>Biblioteca de Assets</h2>
+      <p>Selecione imagens para inserir no template.</p>
+    </div>
+  );
+}
