b8b10fb8ae7e00558d1675ad4e97e88bb7aac902
eadesigners2211-ctrl <eadesigners2211@gmail.com>
2026-09-01 17:32:40 -0300

    feat: add brand settings panel

---
+++ apps/web/src/brand/BrandSettingsPanel.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function BrandSettingsPanel() {
+  return (
+    <aside>
+      <h2>Configurações da Marca</h2>
+      <div>Cores permitidas</div>
+      <div>Fontes permitidas</div>
+      <div>Logo obrigatória</div>
+      <div>Margem mínima</div>
+    </aside>
+  );
+}
