6fe9f6c447dc682f405b46983a549bec0b1110ee
eadesigners2211-ctrl <eadesigners2211@gmail.com>
2026-09-01 17:41:13 -0300

    feat: add bulk creative dashboard foundation

---
+++ apps/web/src/bulk/BulkCreativeDashboard.tsx
@@ -0,0 +1,8 @@
+export function BulkCreativeDashboard() {
+  return (
+    <section>
+      <h1>Bulk Creative Engine</h1>
+      <p>Upload assets, select a template and generate multiple formats.</p>
+    </section>
+  );
+}
