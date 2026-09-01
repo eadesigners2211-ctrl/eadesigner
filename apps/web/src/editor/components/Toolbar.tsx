07272becc21e745ed042210277e14e8c9e853b6e
eadesigners2211-ctrl <eadesigners2211@gmail.com>
2026-09-01 15:52:05 -0300

    feat: add editor toolbar foundation

---
+++ apps/web/src/editor/components/Toolbar.tsx
@@ -0,0 +1,10 @@
+export default function Toolbar(){
+  return (
+    <header>
+      <button>Texto</button>
+      <button>Imagem</button>
+      <button>Forma</button>
+      <button>Salvar</button>
+    </header>
+  );
+}
