94d377e465c8d1d52a56b3ef7e1915b96b4c3b91
eadesigners2211-ctrl <eadesigners2211@gmail.com>
2026-09-01 17:08:31 -0300

    feat: add asset picker component foundation

---
+++ apps/web/src/assets/AssetPicker.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function AssetPicker() {
+  return (
+    <div className="asset-picker">
+      <h3>Selecionar Asset</h3>
+      <p>Biblioteca de arquivos disponível para o editor.</p>
+    </div>
+  );
+}
