51b68276d6d586dbc942f5c62762cd53f28ac15a
eadesigners2211-ctrl <eadesigners2211@gmail.com>
2026-09-01 15:10:16 -0300

    feat: add empty state for asset library

---
+++ apps/web/src/components/AssetEmptyState.tsx
@@ -0,0 +1,8 @@
+export default function AssetEmptyState() {
+  return (
+    <div>
+      <h3>Nenhum asset cadastrado</h3>
+      <p>Envie imagens, logos e elementos gráficos para sua biblioteca.</p>
+    </div>
+  );
+}
