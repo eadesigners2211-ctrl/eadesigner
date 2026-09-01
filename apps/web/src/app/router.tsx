6d671d07da5c8dd6076a599f6a3458a50de9a6ac
eadesigners2211-ctrl <eadesigners2211@gmail.com>
2026-09-01 15:06:15 -0300

    feat: add projects route

--- apps/web/src/app/router.tsx
+++ apps/web/src/app/router.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Login from '../pages/Login';
 import Dashboard from '../pages/Dashboard';
+import Projects from '../pages/Projects';
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/projects" element={<Projects />} />
       <Route path="*" element={<Login />} />
     </Routes>
   );
