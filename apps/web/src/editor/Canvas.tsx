bb13091ff46ac7e0cee0bdf7c09603220901a1fc
eadesigners2211-ctrl <eadesigners2211@gmail.com>
2026-09-02 10:09:15 -0300

    feat(editor): connect canvas selection

--- apps/web/src/editor/Canvas.tsx
+++ apps/web/src/editor/Canvas.tsx
@@ -1,11 +1,21 @@
 import type { EditorState } from './editorState';
+import { selectElement } from './canvasInteraction';
 
-export function Canvas({ state }: { state: EditorState }) {
+export function Canvas({
+  state,
+  onSelect,
+}: {
+  state: EditorState;
+  onSelect?: (id: string) => void;
+}) {
   return (
     <div>
       Canvas {state.canvas.width}x{state.canvas.height}
       {state.elements.map((element) => (
-        <div key={element.id}>
+        <div
+          key={element.id}
+          onClick={() => onSelect?.(selectElement(element.id))}
+        >
           {element.type}: {element.content ?? element.id}
         </div>
       ))}
