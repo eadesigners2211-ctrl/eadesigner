fc1de74bfad00733b338a9df18c7ccb107b0e9db
eadesigners2211-ctrl <eadesigners2211@gmail.com>
2026-09-02 10:06:43 -0300

    Connect toolbar actions with element creation

--- apps/web/src/editor/Toolbar.tsx
+++ apps/web/src/editor/Toolbar.tsx
@@ -1,9 +1,11 @@
-export function Toolbar() {
+import { createImageElement, createShapeElement, createTextElement } from './elementActions';
+
+export function Toolbar({ onAdd }: { onAdd: (element: ReturnType<typeof createTextElement>) => void }) {
   return (
     <nav>
-      <button>Texto</button>
-      <button>Imagem</button>
-      <button>Forma</button>
+      <button onClick={() => onAdd(createTextElement())}>Texto</button>
+      <button onClick={() => onAdd(createImageElement(''))}>Imagem</button>
+      <button onClick={() => onAdd(createShapeElement())}>Forma</button>
     </nav>
   );
 }
